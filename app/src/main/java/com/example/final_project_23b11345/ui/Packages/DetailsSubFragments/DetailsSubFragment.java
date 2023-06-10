4300468d63994ca3df56540290ce48493c9e2615
BenSarkisov <110185655+BenSarks@users.noreply.github.com>
2023-06-10 18:27:46 +0300

    Ui update

--- app/src/main/java/com/example/final_project_23b11345/ui/Packages/DetailsSubFragments/DetailsSubFragment.java
+++ app/src/main/java/com/example/final_project_23b11345/ui/Packages/DetailsSubFragments/DetailsSubFragment.java
@@ -9,7 +9,9 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.final_project_23b11345.Model.Parcel;
 import com.example.final_project_23b11345.R;
 import com.example.final_project_23b11345.Utilities.DataManager;
@@ -20,9 +22,13 @@ import com.google.android.material.textview.MaterialTextView;
 
 public class DetailsSubFragment extends Fragment {
 
-private MaterialTextView sender_Name, delivery_Method, package_Id,order_Date,package_Status,delivery_Date;
+private MaterialTextView sender_Name, delivery_Method, package_Id,order_Date,package_Status,delivery_Date,delivery_Address,ordering_Date;
 
     private FragmentPackageDetailsBinding binding;
+
+    private LottieAnimationView lottieAnimationView;
+
+    private FragmentContainerView fragmentContainerView;
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
                 binding = FragmentPackageDetailsBinding.inflate(inflater, container, false);
@@ -33,12 +39,17 @@ private MaterialTextView sender_Name, delivery_Method, package_Id,order_Date,pac
             Parcel parcel = bundle.getParcelable(PARCEL);
             if (parcel != null) {
                 setParcelDetails(parcel);
-                if (parcel.getPackageStatus().getValue()<5&&parcel.getPackageStatus().getValue()>2){
-                    LatLng userLatlang = DataManager.getInstance().getMyUser().getUserAddressLocation().transformToLatlng();
-                    if(userLatlang!=null){
-                        packagesMapSubFragment.showLocation(userLatlang, parcel.getLocation().transformToLatlng(),parcel.getDeliveryMethod()== Parcel.DeliveryMethod.EXPRESS_COURIER_SHIPPING);
-                    }else{
-                        packagesMapSubFragment.showLocation(null, parcel.getLocation().transformToLatlng(),parcel.getDeliveryMethod()== Parcel.DeliveryMethod.EXPRESS_COURIER_SHIPPING);
+                if(parcel.getPackageStatus().getValue()>4){
+                    lottieAnimationView.setVisibility(View.VISIBLE);
+                    fragmentContainerView.setVisibility(View.GONE);
+                }else{
+                    if (parcel.getPackageStatus().getValue()<5&&parcel.getPackageStatus().getValue()>2){
+                        LatLng userLatlang = DataManager.getInstance().getMyUser().getUserAddressLocation().transformToLatlng();
+                        if(userLatlang!=null){
+                            packagesMapSubFragment.showLocation(userLatlang, parcel.getLocation().transformToLatlng(),parcel.getDeliveryMethod()== Parcel.DeliveryMethod.EXPRESS_COURIER_SHIPPING);
+                        }else{
+                            packagesMapSubFragment.showLocation(null, parcel.getLocation().transformToLatlng(),parcel.getDeliveryMethod()== Parcel.DeliveryMethod.EXPRESS_COURIER_SHIPPING);
+                        }
                     }
                 }
             }
@@ -48,12 +59,16 @@ private MaterialTextView sender_Name, delivery_Method, package_Id,order_Date,pac
     }
 
     private void init(){
+        this.lottieAnimationView = binding.completedParcelAnimation;
+        this.fragmentContainerView =binding.subFragmentMapview;
         this.sender_Name = binding.textviewSenderName;
         this.delivery_Method = binding.textviewPackageMethod;
         this.package_Id = binding.textviewPackageId;
         this.order_Date = binding.textviewPackageDate;
         this.package_Status = binding.textviewPackageStatus;
-        this.delivery_Date = binding.textviewDeliveryDate;
+        this.ordering_Date = binding.textviewPackageDate;
+        this.delivery_Address = binding.textviewDeliveryAddress;
+        this.delivery_Date = binding.textviewRecivedDate;
     }
     public void onDestroyView() {
         super.onDestroyView();
@@ -65,12 +80,15 @@ private MaterialTextView sender_Name, delivery_Method, package_Id,order_Date,pac
         this.sender_Name.setText(parcel.getSendersName());
         this.delivery_Method.setText(requireContext().getResources().getStringArray(R.array.parcel_methods)[parcel.getDeliveryMethod().getValue()]);
         this.package_Id.setText(parcel.getTrackingNumber());
+        this.delivery_Address.setText(parcel.getDeliveryAddress());
         this.order_Date.setText(parcel.getOrderDate());
         this.package_Status.setText(requireContext().getResources().getStringArray(R.array.parcel_status)[parcel.getPackageStatus().getValue()]);
+        this.ordering_Date.setText(parcel.getOrderDate());
         if (parcel.getReceivedDate() == null ){
             this.delivery_Date.setText(R.string.soon);
         }else{
             this.delivery_Date.setText(parcel.getReceivedDate());
+
         }
     }
 }
